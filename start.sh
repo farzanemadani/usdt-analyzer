199854dbd60a46be6074084f177d12748fb7bd97
farzanemadani <farzane.madani@gmail.com>
2026-05-26 13:39:00 +0330

    fix: update start.sh to improve superuser creation and clean up comments

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# migration
 python manage.py migrate
 
-# celery worker در background
-celery -A config worker --loglevel=info &
+# superuser خودکار
+echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'admin@email.com', 'admin123')" | python manage.py shell
 
-# celery beat در background
+celery -A config worker --loglevel=info &
 celery -A config beat --loglevel=info &
 
-# gunicorn اصلی
 gunicorn config.wsgi --log-file -
